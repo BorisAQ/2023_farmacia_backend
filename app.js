3bc6de6d3372ce6ebd30f6efbdbff4b563b8899c
BorisAQ <b_apaza@hotmail.com>
2023-12-25 09:21:08 -0400

    mensaje deploy 31

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ooupxxs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ooupxxs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true ,
+      useUnifiedTopology: true }
+    
+    
     
   )
   .then(() => {
