734a3857405f92f6db6b32ceaed514f4099f8639
BorisAQ <b_apaza@hotmail.com>
2023-12-25 09:04:24 -0400

    mensaje deploy 26

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://borisA:45898@cluster0.ooupxxs.mongodb.net/msg?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ooupxxs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    
   )
   .then(() => {
     app.listen(5000);
